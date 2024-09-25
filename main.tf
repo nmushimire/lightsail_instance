1e87ec419438cf3ae8c8a5e0e0fe7872c746ba7d
nmushimire <mushimirenaomi@gmail.com>
2024-09-24 19:30:23 -0700

    lightsail_code

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_lightsail_instance" "server" {
+    name = "server"
+    blueprint_id = "centos_stream_9"
+    bundle_id = "small_1_0"
+    availability_zone = "us-east-1a"
+    tags = {
+      name="server"
+      env=" test"
+    }
+}
+
+output "server_name" {
+    value = aws_lightsail_instance.server.name
+}
+output "az" {
+    value = aws_lightsail_instance.server.availability_zone
+}
+output "pip" {
+    value = aws_lightsail_instance.server.public_ip_address
+  
+}
